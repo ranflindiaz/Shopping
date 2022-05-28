cdd71997f1ee0f5c1b60b15cef8672718d3dc75b
Ranflin Diaz <ranflindiaz@hotmail.com>
2022-05-28 14:44:19 -0400

    Dashboard Created

---
+++ Shopping/Shopping/Controllers/DashboardController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping.Data;
+using Shopping.Enums;
+using Shopping.Interface;
+
+namespace Shopping.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
+
+        public DashboardController(DataContext context, IUserHelper userHelper)
+        {
+            _context = context;
+            _userHelper = userHelper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.UsersCount = _context.Users.Count();
+            ViewBag.ProductsCount = _context.Products.Count();
+            ViewBag.NewOrdersCount = _context.Orders.Where(o => o.OrderStatus == OrderStatus.New).Count();
+            ViewBag.ConfirmedOrdersCount = _context.Orders.Where(o => o.OrderStatus == OrderStatus.Confirmed).Count();
+
+            return View(await _context.TemporalSales
+                    .Include(u => u.User)
+                    .Include(p => p.Product).ToListAsync());
+        }
+    }
+}
